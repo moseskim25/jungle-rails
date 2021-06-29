108d71a75cafd06ba606517df1768980f7ad038b
Moses Kim <63984177+mosikim@users.noreply.github.com>
2021-06-28 20:21:35 -0400

    tests for user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :name, presence:  true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 8 }
+
+  def self.authenticate_with_credentials(email, password)
+    email_filter = email.downcase.strip
+    @user = User.find_by(email: email_filter).try(:authenticate, password)
+  end 
 end
